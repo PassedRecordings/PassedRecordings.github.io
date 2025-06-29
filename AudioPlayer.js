ce7b76d3baa655347dbf76c9e43f6901c8fbdc8a
On Idyl <zizzapa@gmail.com>
2025-06-28 17:52:37 -0700

    Update AudioPlayer.js - new attempt at a player controller
    Found a potentially better example to try

--- AudioPlayer.js
+++ AudioPlayer.js
@@ -1,4 +1,7 @@
-  <script>
+
+// First attempt - this sort of worked, but relied on having to refresh the page to select a new track
+//Ultimately we should delete this bit down to the next comment
+<script>
     function myFunction() {
        fileArray = ["https://passedrecordingsblog.wordpress.com/wp-content/uploads/2025/06/sublimation.mp3",
                     "https://passedrecordingsblog.wordpress.com/wp-content/uploads/2025/06/in-the-darkness-hope.mp3",
@@ -18,3 +21,43 @@
     myFunction()
 
 </script>
+
+//This one SHOULD go to the next track automagically
+var audio;
+var playlist;
+var tracks;
+var current;
+
+init();
+function init(){
+    current = 0;
+    audio = $('audio');
+    playlist = $('#playlist');
+    tracks = playlist.find('li a');
+    len = tracks.length - 1;
+    audio[0].volume = .10;
+    audio[0].play();
+    playlist.find('a').click(function(e){
+        e.preventDefault();
+        link = $(this);
+        current = link.parent().index();
+        run(link, audio[0]);
+    });
+    audio[0].addEventListener('ended',function(e){
+        current++;
+        if(current == len){
+            current = 0;
+            link = playlist.find('a')[0];
+        }else{
+            link = playlist.find('a')[current];    
+        }
+        run($(link),audio[0]);
+    });
+}
+function run(link, player){
+        player.src = link.attr('href');
+        par = link.parent();
+        par.addClass('active').siblings().removeClass('active');
+        audio[0].load();
+        audio[0].play();
+}
