0da1ae648fef68be370680ad2adb794ecf66b193
PassedRecordings <passedrecordings@gmail.com>
2025-06-30 22:18:25 +0100

    Update Hydra-Sketch2.js

--- Hydra-Sketch2.js
+++ Hydra-Sketch2.js
@@ -1,3 +1,7 @@
-osc()
+s0.initImage("https://i.postimg.cc/rmDMWZCs/Passed-Records-Biege-Single.png")
+b = 0
+update = () => b += 0.05 * Math.sin(time/30)
 
-	.out(o0)
+osc(16,0.1,0.1).hue(0.5,0.7,()=>b)
+  .saturate(()=>b/5)
+  .pixelate(16,40).modulateKaleid(src(s0),64).out()
