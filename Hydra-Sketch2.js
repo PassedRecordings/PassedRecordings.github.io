b636123a5b88c40a8cdc35c0827b702c68b0de2a
PassedRecordings <passedrecordings@gmail.com>
2025-06-30 22:04:45 +0100

    Update Hydra-Sketch2.js

--- Hydra-Sketch2.js
+++ Hydra-Sketch2.js
@@ -1,9 +1,9 @@
-noise()
+  noise()
 	.blend(osc()
 		.modulate(noise()), () => Math.cos(((time / 10) + 1) / 2))
 .modulateKaleid(osc(20).modulate(noise(3)))
 	.colorama(() => Math.sin(((time / 25) + 1) / 2)*0.25)
-.contrast(()=>(Math.sin(time/3+1)/2)*4)
+.contrast(()=>(Math.sin((time/4)+1.4)/2))
 .add(gradient().rotate(()=>time/10),0.2)
 
 	.out()
